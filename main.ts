653eba66ab687b07973969221dc847d8f7340937
SrAquaGenius <dimoisivic@gmail.com>
2024-02-13 22:45:30 +0000

    Base scheme for the plugin

---
+++ main.ts
@@ -0,0 +1,21 @@
+import { Plugin } from 'obsidian';
+
+export default class CustomFileClass extends Plugin {
+
+	async onload(): Promise<void>
+	{
+
+	}
+
+	/*
+	------------
+	Settings
+	------------
+	*/
+
+	async loadSettings() {};
+
+	async saveSettings() {};
+
+	onunload() {};
+}
