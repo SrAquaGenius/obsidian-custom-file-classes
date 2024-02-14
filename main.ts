8978dad9053440efef1391cc84ab390c206c0a82
SrAquaGenius <dimoisivic@gmail.com>
2024-02-14 10:07:37 +0000

    Added main.js

--- main.ts
+++ main.ts
@@ -1,10 +1,21 @@
-import { Plugin } from 'obsidian';
+import { Notice, Plugin } from 'obsidian';
 
 export default class CustomFileClass extends Plugin {
 
 	async onload(): Promise<void>
 	{
-
+		console.log("---------- Custom File Classes Obsidian loading ----------");
+		
+		if (!this.app.plugins.enabledPlugins.has("dataview") || (
+			//@ts-ignore
+			this.app.plugins.plugins["dataview"] && !this.app.plugins.plugins["dataview"].settings.enableDataviewJs)
+		) {
+			new Notice(
+				`------------------------------------------\n` +
+				`(!) INFO (!) \n` +
+				`Install and enable dataview and dataviewJS for extra Metadata Menu features\n` +
+				`------------------------------------------`, 60000)
+		}
 	}
 
 	/*
